99f8d31f4f7dc25eade1c7acbfb167eafb448baf
geraldsberongoy <geraldsberongoy@iskolarngbayan.pup.edu.ph>
2024-01-20 23:35:28 +0800

    pseudocode

--- print_characs_in_str_at_even_index_number.py
+++ print_characs_in_str_at_even_index_number.py
@@ -1,2 +1,6 @@
 # Exercise 3: Print characters from a string that are present at an even index number
 # Write a program to accept a string from the user and display characters that are present at an even index number.
+
+# pseudocode
+# user input
+# print(user[0::2])
